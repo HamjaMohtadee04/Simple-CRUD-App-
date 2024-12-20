17a5e17674561952ddb6f36473682e56da6e486c
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2024-12-21 02:05:53 +0600

    routing added to the app and so that index.js code organized for routing functionality

--- index.js
+++ index.js
@@ -1,75 +1,82 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const Product = require('./models/product.model.js')
+const productRoute =require('./routes/product.route.js')
 const app = express()
 
+//middleWare
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+//routes
+app.use("/api/products",productRoute)
+
+
 
 app.get('/', (req, res) => {
     res.send("hello from node api updated");
 })
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+// app.get('/api/products', async (req, res) => {
+//     try {
+//         const products = await Product.find({})
+//         res.status(200).json(products)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
 
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+// app.get('/api/products/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// })
 
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+// app.post('/api/products', async (req, res) => {
+//     try {
+//         const product = await Product.create(req.body)
+//         res.status(200).json(product)
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
 
-//update a product
-app.put('/api/product/:id', async (req, res) => {
+// //update a product
+// app.put('/api/products/:id', async (req, res) => {
 
-    try {
-        const { id } = req.params
-        const product = await Product.findByIdAndUpdate(id, req.body)
-        if (!product) {
-            return res.status(404).json({ message: 'product not found' })
-        }
-        const updateProduct = await Product.findById(id)
-        res.status(200).json(updateProduct)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+//     try {
+//         const { id } = req.params
+//         const product = await Product.findByIdAndUpdate(id, req.body)
+//         if (!product) {
+//             return res.status(404).json({ message: 'product not found' })
+//         }
+//         const updateProduct = await Product.findById(id)
+//         res.status(200).json(updateProduct)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
 
 
-//delete a product
-app.delete('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findByIdAndDelete(id)
-        if (!product) {
-            return res.status(404).json({ message: 'product not found' })
-        }
-        res.status(200).json({ message: "product has been deleted successfully" });
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+// //delete a product
+// app.delete('/api/products/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params
+//         const product = await Product.findByIdAndDelete(id)
+//         if (!product) {
+//             return res.status(404).json({ message: 'product not found' })
+//         }
+//         res.status(200).json({ message: "product has been deleted successfully" });
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
 
 
 mongoose.connect("mongodb+srv://hamzamohtadi225:12345@cluster0.wayv5.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
